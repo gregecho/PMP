9eb616e342731068592fb81297ed8bc929085027
gregecho <greg_echo@hotmail.com>
2017-10-16 11:07:45 +0800

    Initialize FreeMind.

---
+++ PMP概览导图.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1508122569862" ID="ID_992806501" MODIFIED="1508122633849" TEXT="PMP&#x6982;&#x89c8;"/>
+</map>
---
+++ 项目整合管理导图.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1508123132567" ID="ID_340890269" MODIFIED="1508123141719" TEXT="&#x9879;&#x76ee;&#x6574;&#x5408;&#x7ba1;&#x7406;"/>
+</map>
